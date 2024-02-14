f790e02f7c417cf8bd7139b4cd86a5985d1ec40a
vhronsz <ryansanjaya290799@gmail.com>
2024-02-14 23:11:26 +0700

    add cors permission

--- routes/auth.js
+++ routes/auth.js
@@ -1,2 +1,16 @@
 var express = require('express');
 var router = express.Router();
+
+router.get("/", function(req,res,next){
+    console.log(req);
+    res.send("testing");
+});
+
+
+router.post("/login", function(req,res,next){
+    console.log(req.body);
+    res.send("Logged in");
+});
+
+
+module.exports = router;
