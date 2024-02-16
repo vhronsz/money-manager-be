333485a6babd8b8948563db983f656f10a1a0416
vhronsz <ryansanjaya290799@gmail.com>
2024-02-16 20:31:20 +0700

    add placeholder login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", function(req,res,next){
-    console.log(req);
+    const username = req.params.username;
+    const password = req.params.password;
+    console.log({username, password});
     res.send("testing");
 });
-
 
 router.post("/login", function(req,res,next){
     console.log(req.body);
     res.send("Logged in");
 });
-
 
 module.exports = router;
