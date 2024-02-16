6039d6c14bea21842ad95f9361e40dfcc2e85194
Ryan Sanjaya <ryansanjaya290799@gmail.com>
2024-02-16 23:25:40 +0700

    asd

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var authRouter = require('./routes/auth');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/auth', authRouter);
+app.use('/',indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
