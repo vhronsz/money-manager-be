6fc42fa8a14ac76b6d683b223f2056c923f6048f
vhronsz <ryansanjaya290799@gmail.com>
2024-03-19 11:04:08 +0700

    change on router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send("testing");
+router.get("/", function(req,res,next){
+  res.json({
+      type: "debug",
+      response: {
+          message: "testing /"
+      }
+  });
 });
 
 module.exports = router;
