492109314a37780254dd33bda9e30b574d13bc6c
vhronsz <ryansanjaya290799@gmail.com>
2024-04-18 14:56:01 +0700

    cancel use of sequelize

--- model/User.js
+++ model/User.js
@@ -1,12 +1,2 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
-
-const sequelize = new Sequelize('sqlite::memory');
-
-class User extends Model{}
-
-User.init({
-    name:{},
-    username:{},
-    password:{},
-    email:{}
-});
+//Buat Class biasa
+class User{ }
