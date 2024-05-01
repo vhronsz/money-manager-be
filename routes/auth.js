66b5cfdecdb5a5b3c959c3a9f51b135e0440904e
vhronsz <ryansanjaya290799@gmail.com>
2024-05-01 22:40:13 +0700

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,40 +1,26 @@
+require("dotenv").config();
+
 var express = require('express');
 var router = express.Router();
 var axios = require("axios");
 
-///
-var UserRepository = require("../repository/users_repository");
-var ResponseDto = require("../dto/responseDto");
-
-
 router.post("/login", async function (req, res, next) {
-
-    try{
-    let isAuthenticated = false;
-    let response = new ResponseDto();
-
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const user = await axios.post(
-        "http://localhost:9200/auth/login",
-        {
-            username: username,
-            password: password
-        }
-    );
-
-
-    if (isAuthenticated) {
-        response.type = "SUCCESS";
-        response.message = "Login Success";
-        response.data = {};
-        return res.json(json)
-    }
-    }catch(err){
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+        const URL = `${process.env.AUTH_URL}/auth/login`;
+        const response = await axios.post(
+            URL,
+            {
+                username: username,
+                password: password
+            }
+        )
+        return res.json(response.data);
+    } catch (err) {
         console.log(err);
         next(err);
     }
-});
+}); 
 
 module.exports = router;
