e115aa8f65cbe6199339bd1b296e8d04df0016ce
vhronsz <ryansanjaya290799@gmail.com>
2024-05-01 14:40:14 +0700

    update: send request to auth service

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var axios = require("axios");
 
+///
 var UserRepository = require("../repository/users_repository");
 var ResponseDto = require("../dto/responseDto");
 
-router.post("/login", function (req, res, next) {
-    let isAuthenticated = false;
-    let response = new ResponseDto();
 
-    const username = req.body.username;
-    const password = req.body.password;
+router.post("/login", async function (req, res, next) {
+    try {
+        let isAuthenticated = false;
+        let response = new ResponseDto();
 
-    response.type = "SUCCESS";
-    response.message = "Login Success";
-    response.data = {};
-    return res.json({
-        ...response
-    })
+        const username = req.body.username;
+        const password = req.body.password;
+
+        console.log("username");
+        const login = await axios.post("http://localhost:9200/auth/login", { username: username, password: password });
+        if (isAuthenticated) {
+            response.type = "SUCCESS";
+            response.message = "Login Success";
+            response.data = {};
+            return res.json(json)
+        }
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
